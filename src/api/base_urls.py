ce44b509cac97ac84411588d39f4f17b2945ce6c
brian-mati <brianmuthomimati@gmail.com>
2025-02-23 13:32:52 +0300

    added base poke api urls to base_urs.py file

---
+++ src/api/base_urls.py
@@ -0,0 +1,5 @@
+
+pokemon_color = 'https://pokeapi.co/api/v2/pokemon-species/'
+pokemon_ability_name = 'https://pokeapi.co/api/v2/pokemon/'
+pokemon_ability_description = 'https://pokeapi.co/api/v2/ability/'
+pokemon_gender_rate = 'https://pokeapi.co/api/v2/pokemon-species/'
